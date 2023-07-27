bf421bfe81862e9d9dc77d39c478a06e1f60aa08
Khaya Kingston <khaya.kingston@gmail.com>
2023-07-26 18:30:54 -0700

    current iteration totally completed

--- backend.js
+++ backend.js
@@ -1,8 +1,6 @@
 // Basic operate function
 function operate(op, num1, num2) {
 
-    console.log('operator Called');
-
     switch (op) {
         case '+':
             return String(Number(num1)+Number(num2));
@@ -20,7 +18,7 @@ function operate(op, num1, num2) {
             return String(Number(num1)/Number(num2));
             break;
 
-        case 'exp':
+        case '^':
             return String(Number(num1)**Number(num2));
             break;
 
@@ -35,28 +33,18 @@ const screen = document.querySelector('#screen');
 const upper = screen.querySelector('#work-holder')
 const disp = screen.querySelector('#result-display');
 
-// Operation List
-const ops = ['+', '-', '*', '/', 'exp', 'sqrt', 'fact'];
-
+// hotkey keycode dictionary
+const specialHotKeys = {'±':'negate', '%':'perc', '^':'exp',
+                        '@':'root', 'Enter':'equals', '.':'point',
+                        '+':'+', '*':'*', '-':'-', '/':'/',
+                        'c':'clear', 'Backspace':'del'};
 
 // Calculator Stack
 let calc = ['0'];
 disp.textContent = calc[calc.length - 1];
 
-
-// Errors
-const errors = {
-    intError : "Error: Must Start With Num",
-    opError : "Error: Can't Concat Operations",
-    ptError : "Error: Number Has 2 Points",
-    negError : "Error: Can't Negate Nothing"
-}
-
-// Boolean for checking decimal nature
-let isDec = false;
-
+// Simplifies the calc stack with its existing operations
 function simplify() {
-    console.log('simplify Called');
     if (calc.length === 1) {
         return;
     } else {
@@ -67,8 +55,8 @@ function simplify() {
     }
 }
 
+// Either concatenates or adds numbers to the calculator (or negates)
 function numFiller(e) {
-    console.log('numFiller Called');
     if (e.target.id != 'negate') {
         isNaN(Number(calc[calc.length - 1])) ? calc.push(e.target.textContent) : 
         calc[calc.length - 1] += e.target.textContent;
@@ -81,8 +69,8 @@ function numFiller(e) {
     upper.textContent = calc.join(' ');
 }
 
+// Decides what to do with a number input
 function numRunner(e) {
-    console.log('numRunner Called');
     calc[0] === '0' && upper.textContent === '' ? (() => {
     if (e.target.id != 'negate') {
         calc[0] = e.target.textContent; 
@@ -92,8 +80,9 @@ function numRunner(e) {
     })() : numFiller(e);
 }
 
+
+// Decides what to do with an operation input
 function opRunner(e) {
-    console.log('opRunner Called');
     if (e.target.id === 'equals') {
         if (isNaN(Number(calc[calc.length - 1]))) {
             let popper = calc.pop();
@@ -102,8 +91,23 @@ function opRunner(e) {
         upper.textContent += ' =';
         simplify();
         disp.textContent = calc[0];
+    } else if (e.target.id === 'perc') {
+        if (isNaN(Number(calc[calc.length - 1]))) {
+            let popper = calc.pop();
+        } 
+        calc[calc.length-1] = String(Number(calc[calc.length-1]/100));
+        upper.textContent = calc.join(' ');
+        disp.textContent = calc[-1];
+    } else if (e.target.id === 'root') {
+        if (isNaN(Number(calc[calc.length - 1]))) {
+            let popper = calc.pop();
+        } 
+        calc[calc.length-1] = String(Number(calc[calc.length-1]**0.5));
+        upper.textContent = calc.join(' ');
+        disp.textContent = calc[-1];
     }
     else {
+        let toAdd = e.target.textContent;
         isNaN(Number(calc[calc.length - 1])) ? (() => {
             calc[calc.length - 1] = e.target.textContent;
         })() : (() => {
@@ -115,6 +119,7 @@ function opRunner(e) {
     }
 }
 
+// Decides what to do with a del-clear input
 function powRunner(e) {
     
     if (e.target.textContent === 'AC') {
@@ -136,14 +141,14 @@ function powRunner(e) {
 
 }
 
-
+// Handles where to send an input based on button
 function buttonPress(e) {
-    console.log('Button Pressed');
     e.target.classList[0] === "del-clear" ? powRunner(e) : (() => {
         e.target.classList[0] === "num-arg" ? numRunner(e) : opRunner(e);
     })()
 }
 
+// Extracts the buttons on the calc and adds the click event listeners
 const buttons = document.getElementsByTagName('button');
 
 buttonsArr = [...buttons]
@@ -152,7 +157,17 @@ buttonsArr.forEach(button => {
     button.addEventListener('click', buttonPress);
 });
 
-console.log(calc);
-console.log(disp);
-console.log(upper);
-console.log(isDec);
+
+// Basically just checks if the key you pressed is mappable to a button
+// And if so it activates it
+document.addEventListener('keydown', (e) => {
+    let currButton = null;
+    if (e.key in specialHotKeys) {
+        currButton = document.getElementById(specialHotKeys[e.key]);
+        currButton.click();
+    } else if (e.key >= '0' && e.key <= '9') {
+        currButton = document.getElementById(e.key);
+        currButton.click();
+    } 
+})
+
